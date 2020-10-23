ae4a7976e894bbfaf42bcb6226c6382cdf288d5e
Luke-McShane <lukejsmcshane@gmail.com>
2020-10-24 08:56:44 +1100

    Commented modal code

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ let isSetup = false;
 
 window.onload = function () {
 
+  let closeOverlay = document.querySelectorAll(".close");
+  // These headers are used for the subheadings inside the modal
+  let cssHeaders = ['CSS Projects', 'CSS Exercises'];
+  let jsHeaders = ['JS Projects', 'JS Exercises'];
+  let exercisesBtns = document.querySelectorAll(`[class*="exercises-main-container-button"]`);
+
+  // This sets up the subheadings for the respective section
+  // If the subheading(s) is already setup, the function is not run
+  // This check is necessary, because the user can close and reopen the modal, and the
+  // modal needs to be re setup each time
+  function modalSetup(h3, key, isSetup = false) {
+    if (!isSetup) {
+      h3.innerText = key;
+      h3.style.textAlign = 'center';
+      h3.style.marginTop = '2rem';
+      document.querySelector('.exercises-info-content-grid-text').appendChild(h3);
+    }
+  }
+
   function populateModal(title, link) {
     const a = document.createElement('a');
     const div = document.createElement('div');
@@ -26,32 +45,20 @@ window.onload = function () {
     div.appendChild(hr);
   }
 
-  // function modalSetup(h3, key, h2 = false, header = '') {
-  //   if (h2) {
-  //     h2.innerText = header;
-  //     h2.classList.add('modal-title');
-  //     document.querySelector('.exercises-info-content-grid').insertBefore(h2, document.querySelector('.exercises-info-content-grid-text'));
-  //   }
-  //   h3.innerText = key;
-  //   h3.style.textAlign = 'center';
-  //   h3.style.marginTop = '2rem';
-  //   document.querySelector('.exercises-info-content-grid-text').appendChild(h3);
-  // }
-  function modalSetup(h3, key, isSetup = false) {
-    if (!isSetup) {
-      h3.innerText = key;
-      h3.style.textAlign = 'center';
-      h3.style.marginTop = '2rem';
-      document.querySelector('.exercises-info-content-grid-text').appendChild(h3);
-    }
-  }
-
   scroll();
   freezeForTransition();
 
-  let closeOverlay = document.querySelectorAll(".close");
+  // Here we creat functionality for closing both of the overlays - one for the Projects section,
+  // the other for the Exercises section.
   closeOverlay.forEach(btn => {
+
+    // For the Projects section, we simply change the display setting to 'none' and
+    // remove the 'show-overlay' class, and finally scroll the window to the appropriate position
+    // However, for the Exercises section, we must remove all elements we added to the modal.
+    // This is because the modal is newly generated each time it is shown, meaning that, if we did
+    // not remove the elements, they would stack
     btn.onclick = function () {
+
       let overlay = (btn.parentElement.id == "projects-cancel") ?
         document.querySelector(".projects-info") :
         document.querySelector(".exercises-info");
@@ -67,37 +74,48 @@ window.onload = function () {
     }
   })
 
-  let exercisesBtns = document.querySelectorAll(`[class*="exercises-main-container-button"]`);
-  let cssHeaders = ['CSS Projects', 'CSS Exercises'];
-  let jsHeaders = ['JS Projects', 'JS Exercises'];
-  for (let i = 0; i < exercisesBtns.length; i++) {
-    let currentBtn = exercisesBtns[i];
 
+  // Each exerciseBtn, when clicked, will run code that causes the modal overlay to dynamically
+  // populate (based on the button clicked) and then display the correspending data to the user
+  for (let i = 0; i < exercisesBtns.length; i++) {
+
+    let currentBtn = exercisesBtns[i];
     currentBtn.onclick = function () {
-      isSetup = false;
+      const h2 = document.createElement('h2');
       let classes = currentBtn.classList.value;
+      // This infoKey will be used to select the corresponding data in the js object found in
+      // the codepen-info.js file. The value of infoKey depends on the button clicked, for each
+      // button has class data corresponding to the data we want selected
       let infoKey = (
         classes.includes('html') ? 'html' :
           classes.includes('css') ? 'css' :
             classes.includes('js') ? 'js' :
               null
       );
-      const h2 = document.createElement('h2');
+      // isSetup is reset each time the modal is shown, and is set to 'true' if the js object
+      // is one layer deep (no nested objects), which is the case for the HTML data, for example
+      isSetup = false;
+      // Setting up the main header for the modal and adding it to the HTML structure
       h2.innerText = `My ${infoKey.toUpperCase()} Work`;
       h2.classList.add('modal-title');
       document.querySelector('.exercises-info-content-grid').insertBefore(h2, document.querySelector('.exercises-info-content-grid-text'));
 
-      console.log(`Info Key: ${infoKey}`);
+      // Find the scroll position to prevent the page moving to the top after the modal is closed.
       scrollPosition = window.pageYOffset;
       document.body.style.top = -scrollPosition + 'px';
+      // Add the class that causes the the core of the webpage to display as fixed, meaning the user,
+      // when scrolling, will not be scrolling through the main webpage, but only through the overlay
       document.body.classList.add('show-overlay');
 
+      // Display the overlay and bring in the data from the codepen-info.js object
       let overlay = document.getElementsByClassName("exercises-info")[0];
       let exercisesInfo = codepenData.default;
       overlay.style.display = "flex";
 
+      // Loop through the corresponding object data
       for (const [tempKey, tempVal] of Object.entries(exercisesInfo[`${infoKey}`])) {
 
+        // Set the key which will be used to determine the subheading
         const key = (
           infoKey === 'html' ? 'HTML Projects' :
             infoKey === 'css' ? cssHeaders[tempKey] :
@@ -106,26 +124,32 @@ window.onload = function () {
         );
         const val = Object.values(tempVal)[0];
         const h3 = document.createElement('h3');
+        // If we have nested objects within the current subheading..
         if (typeof (Object.values(val[0])[0]) === 'object') {
           modalSetup(h3, key);
+          // Loop through each of these inner objects..
           Object.entries(val[0]).forEach(entry => {
+            // Creating both a subheading for this inner-object..
             const innerH3 = document.createElement('h3');
             modalSetup(innerH3, Object.values(entry)[0]);
+            // And looping through the inner object to populate the modal with the innermost data
             for (const innerVal of Object.values(entry[1])) {
               console.log(innerVal);
               populateModal(innerVal.title, innerVal.link);
             }
           });
 
+          // If there is only only one sub layer of objects, as is the case with the CSS data..
         } else if (typeof (val) === 'object') {
-          debugger;
           modalSetup(h3, key);
-          console.log(`Inner Key: ${key}`);
-          console.log(`Inner Value: ${val}`);
+          // Loop through each inner object and populate the modal accordingly
           val.forEach(entry => populateModal(entry.title, entry.link));
         } else if (typeof (val) === 'string') {
           modalSetup(h3, key, isSetup);
           isSetup = true;
+          // Else, we need not loop through anything internally, for there are no objects to
+          // loop through, and may just populate this part of the modal as such then proceed
+          // to the next title/link pair
           populateModal(tempVal.title, tempVal.link);
         }
       }
