fd42ee8057b52e4204a5454cb99d907475e01da6
Luke-McShane <lukejsmcshane@gmail.com>
2020-10-24 12:44:48 +1100

    More updates to codepen-info

--- codepen-info.js
+++ codepen-info.js
@@ -3,141 +3,349 @@ export default {
     {
       "JS CodePen Projects": [
         {
-          "title": "Please tell me you like this project, will you? Really does me esteem good.",
-          "link": "a"
+          "title": "01 - Password Validator",
+          "link": "https://codepen.io/Luke-McShane/pen/mdEPNzq"
         },
         {
-          "title": "Please tell me you like this project, will you? Really does me esteem good.",
-          "link": "b"
+          "title": "02 - Average Value",
+          "link": "https://codepen.io/Luke-McShane/pen/OJXNKBZ"
         },
         {
-          "title": "Please tell me you like this project, will you? Really does me esteem good.",
-          "link": "c"
+          "title": "03 - Pangrams",
+          "link": "https://codepen.io/Luke-McShane/pen/pobyMxB"
         },
         {
-          "title": "Please tell me you like this project, will you? Really does me esteem good.",
-          "link": "d"
+          "title": "04 - Get Playing Card",
+          "link": "https://codepen.io/Luke-McShane/pen/MWeyNPM"
         },
         {
-          "title": "Please tell me you like this project, will you? Really does me esteem good.",
-          "link": "e"
+          "title": "05 - Annoying 'this' Arrow Function",
+          "link": "https://codepen.io/Luke-McShane/pen/rNLeXQN"
         },
         {
-          "title": "Please tell me you like this project, will you? Really does me esteem good.",
-          "link": "f"
+          "title": "06 - Deck of Cards",
+          "link": "https://codepen.io/Luke-McShane/pen/JjKXgeK"
+        },
+        {
+          "title": "07 - Annoying Button",
+          "link": "https://codepen.io/Luke-McShane/pen/dyXMxQv"
         }
       ],
     },
     {
       "JS CodePen Exercises": [
         {
-          "Section 1: asdf": [
+          "Section 1: Controlling Program Logic and Flow": [
             {
-              "title": "Section 1 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "1.1 - If/Else",
+              "link": "https://codepen.io/Luke-McShane/pen/YzWqMwW"
             },
             {
-              "title": "Section 1 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "1.2 - String Literals",
+              "link": "https://codepen.io/Luke-McShane/pen/gOMrNGa"
             },
             {
-              "title": "Section 1 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "1.3 - Nested 'if' Statements",
+              "link": "https://codepen.io/Luke-McShane/pen/KKMzjXv"
             },
             {
-              "title": "Section 1 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "1.4 - 'truthy' & 'falsey'",
+              "link": "https://codepen.io/Luke-McShane/pen/zYBqVPL"
             },
             {
-              "title": "Section 1 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "1.5 - Operator Precedence",
+              "link": "https://codepen.io/Luke-McShane/pen/dyXMBZE"
             },
             {
-              "title": "Section 1 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "1.6 - The 'switch' Statement",
+              "link": "https://codepen.io/Luke-McShane/pen/mdEPZpy"
+            },
+            {
+              "title": "1.7 - The Ternary Operator",
+              "link": "https://codepen.io/Luke-McShane/pen/BazKgYL"
             }
           ],
 
-          "Section 2: asdf": [
+          "Section 2: Capturing Collections of Data": [
             {
-              "title": "Section 2 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "2.1 - Array Basics",
+              "link": "https://codepen.io/Luke-McShane/pen/ZEOWdrx"
             },
             {
-              "title": "Section 2 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "2.2 More Array Methods",
+              "link": "https://codepen.io/Luke-McShane/pen/LYZNKQv"
             },
             {
-              "title": "Section 2 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 2 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 2 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 2 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "2.3 - Reference Types",
+              "link": "https://codepen.io/Luke-McShane/pen/mdEPZLJ"
             }
           ],
 
-          "Section 3: asdf": [
+          "Section 3: Objects": [
             {
-              "title": "Section 3 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "3.1 - Object Property Access",
+              "link": "https://codepen.io/Luke-McShane/pen/pobyXVd"
             },
             {
-              "title": "Section 3 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 3 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 3 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 3 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 3 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "3.2 - Object Basics",
+              "link": "https://codepen.io/Luke-McShane/pen/wvWGLjo"
             }
           ],
 
-          "Section 4: asdf": [
+          "Section 4: Loops": [
             {
-              "title": "Section 4 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "4.1 - For Loops",
+              "link": "https://codepen.io/Luke-McShane/pen/zYBqVjm"
             },
             {
-              "title": "Section 4 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "4.2 - While Loops",
+              "link": "https://codepen.io/Luke-McShane/pen/VwjaJdZ"
             },
             {
-              "title": "Section 4 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "4.3 - 'for..of...' Loops",
+              "link": "https://codepen.io/Luke-McShane/pen/BazKgVQ"
             },
             {
-              "title": "Section 4 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 4 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
-            },
-            {
-              "title": "Section 4 is a lovely test title. You do think it's lovely, right..?",
-              "link": ""
+              "title": "4.4 - 'for..in...' Loops",
+              "link": "https://codepen.io/Luke-McShane/pen/jOrqjKG"
             }
           ],
+
+          "Section 5: Functions": [
+            {
+              "title": "5.1 - Function Basics",
+              "link": "https://codepen.io/Luke-McShane/pen/zYBqVjm"
+            }
+          ],
+
+          "Section 6: An Advanced Look at Functions": [
+            {
+              "title": "6.1 - Scope",
+              "link": "https://codepen.io/Luke-McShane/pen/abZNgKP"
+            },
+            {
+              "title": "6.2 - Function Expressions",
+              "link": "https://codepen.io/Luke-McShane/pen/VwjaJBZ"
+            },
+            {
+              "title": "6.3 - Functions as Methods",
+              "link": "https://codepen.io/Luke-McShane/pen/OJXNewR"
+            },
+            {
+              "title": "6.4 - Higher Order Functions",
+              "link": "https://codepen.io/Luke-McShane/pen/jOrqjpG"
+            },
+            {
+              "title": "6.5 - Returning Functions",
+              "link": "https://codepen.io/Luke-McShane/pen/MWeyMBV"
+            },
+            {
+              "title": "6.6 - Callback Functions",
+              "link": "https://codepen.io/Luke-McShane/pen/XWKdLBP"
+            },
+            {
+              "title": "6.7 - Hoisting",
+              "link": "https://codepen.io/Luke-McShane/pen/dyXMBjx"
+            }
+          ],
+
+          "Section 7: Applying Functions to Collections of Data": [
+            {
+              "title": "7.1 - 'forEach'",
+              "link": "https://codepen.io/Luke-McShane/pen/GRqZbXo"
+            },
+            {
+              "title": "7.2 - Map",
+              "link": "https://codepen.io/Luke-McShane/pen/wvWGLEg"
+            },
+            {
+              "title": "7.3 - Arrow Functions",
+              "link": "https://codepen.io/Luke-McShane/pen/WNxwqgO"
+            },
+            {
+              "title": "7.4 - Implicit Return",
+              "link": "https://codepen.io/Luke-McShane/pen/KKMzjxy"
+            },
+            {
+              "title": "7.5 - Find",
+              "link": "https://codepen.io/Luke-McShane/pen/MWeyMqV"
+            },
+            {
+              "title": "7.6 - Filter",
+              "link": "https://codepen.io/Luke-McShane/pen/XWKdLPx"
+            },
+            {
+              "title": "7.7 - 'every' and 'some",
+              "link": "https://codepen.io/Luke-McShane/pen/qBNZzMG"
+            },
+            {
+              "title": "7.8 - Sort",
+              "link": "https://codepen.io/Luke-McShane/pen/bGepPmW"
+            },
+            {
+              "title": "7.9 - Reduce",
+              "link": "https://codepen.io/Luke-McShane/pen/QWENXZq"
+            }
+          ],
+
+          "Section 8: Miscellaneous JavaScript Features": [
+            {
+              "title": "8.1 - Default Parameters",
+              "link": "https://codepen.io/Luke-McShane/pen/eYzZwPj"
+            },
+            {
+              "title": "8.2 - Spread",
+              "link": "https://codepen.io/Luke-McShane/pen/ExyKBdG"
+            },
+            {
+              "title": "8.3 - Spreading Array Literals",
+              "link": "https://codepen.io/Luke-McShane/pen/gOMrNQM"
+            },
+            {
+              "title": "8.4 - Spreading Object Literals",
+              "link": "https://codepen.io/Luke-McShane/pen/wvWGLQJ"
+            },
+            {
+              "title": "8.5 - Rest Parameters",
+              "link": "https://codepen.io/Luke-McShane/pen/jOrqjQa"
+            },
+            {
+              "title": "8.6 - Destructuring Arrays",
+              "link": "https://codepen.io/Luke-McShane/pen/yLJOdmE"
+            },
+            {
+              "title": "8.7 - Destructuring Objects",
+              "link": "https://codepen.io/Luke-McShane/pen/JjKXgEo"
+            },
+            {
+              "title": "8.8 - Nested Destructuring",
+              "link": "https://codepen.io/Luke-McShane/pen/PozNMWK"
+            },
+            {
+              "title": "8.9 - Destructuring Parameters",
+              "link": "https://codepen.io/Luke-McShane/pen/WNxwVRJ"
+            }
+          ],
+
+          "Section 9: Object Methods and the 'this' Keyword": [
+            {
+              "title": "9.1 - Shorthand Object Properties",
+              "link": "https://codepen.io/Luke-McShane/pen/NWrNQpY"
+            },
+            {
+              "title": "9.2 - Computed Properties",
+              "link": "https://codepen.io/Luke-McShane/pen/jOrqgBp"
+            },
+            {
+              "title": "9.3 - Object Methods",
+              "link": "https://codepen.io/Luke-McShane/pen/zYBqgZg"
+            },
+            {
+              "title": "9.4 - The 'this' Keyword",
+              "link": "https://codepen.io/Luke-McShane/pen/RwRaXVp"
+            },
+            {
+              "title": "9.5 - 'this' Invokation",
+              "link": "https://codepen.io/Luke-McShane/pen/WNxwVOy"
+            }
+          ],
+
+          "Section 10: DOM Manipulation": [
+            {
+              "title": "10.1 - Basic Manipulation",
+              "link": "https://codepen.io/Luke-McShane/pen/LYZNwzN"
+            },
+            {
+              "title": "10.2 - 'getElementById'",
+              "link": "https://codepen.io/Luke-McShane/pen/QWENeqV"
+            },
+            {
+              "title": "10.3 - 'getElementsByTagName'",
+              "link": "https://codepen.io/Luke-McShane/pen/ExyKqbx"
+            },
+            {
+              "title": "10.4 - 'getElementsByClassName'",
+              "link": "https://codepen.io/Luke-McShane/pen/qBNZeVb"
+            },
+            {
+              "title": "10.5 - 'querySelector'",
+              "link": "https://codepen.io/Luke-McShane/pen/YzWqmEN"
+            }
+          ],
+
+          "Section 11: Styling DOM Elements": [
+            {
+              "title": "11.1 - 'innerText' & 'textContent'",
+              "link": "https://codepen.io/Luke-McShane/pen/JjKXgMY"
+            },
+            {
+              "title": "11.2 - 'innerHTML'",
+              "link": "https://codepen.io/Luke-McShane/pen/vYKGopx"
+            },
+            {
+              "title": "11.3 - Editing 'val', 'src', etc.",
+              "link": "https://codepen.io/Luke-McShane/pen/wvWGVpm"
+            },
+            {
+              "title": "11.4 - 'getAttribute' & 'setAttribute'",
+              "link": "https://codepen.io/Luke-McShane/pen/KKMzOZr"
+            },
+            {
+              "title": "11.5 - Parent, Children, & Sibling Elements",
+              "link": "https://codepen.io/Luke-McShane/pen/MWeyNrx"
+            },
+            {
+              "title": "11.6 - Changing Multiple Elements",
+              "link": "https://codepen.io/Luke-McShane/pen/eYzZqyw"
+            },
+            {
+              "title": "11.7 - Changing Styles",
+              "link": "https://codepen.io/Luke-McShane/pen/XWKdvZr"
+            },
+            {
+              "title": "11.8 = 'getComputedStyle'",
+              "link": "https://codepen.io/Luke-McShane/pen/YzWqmeZ"
+            },
+            {
+              "title": "11.9 - Manipulating Classes",
+              "link": "https://codepen.io/Luke-McShane/pen/PozNMQE"
+            },
+            {
+              "title": "11.10 - Creating Elements",
+              "link": "https://codepen.io/Luke-McShane/pen/BazKXYr"
+            },
+            {
+              "title": "11.11 - 'append', 'prepend', & 'insertBefore'",
+              "link": "https://codepen.io/Luke-McShane/pen/JjKXgLG"
+            },
+            {
+              "title": "11.12 - 'removeChild' & 'remove'",
+              "link": "https://codepen.io/Luke-McShane/pen/vYKGoRm"
+            }
+          ],
+
+          "Section 12: Communicating with Events": [
+            {
+              "title": "12.1 - Introduction to Events",
+              "link": "https://codepen.io/Luke-McShane/pen/ExyKqLa"
+            },
+            {
+              "title": "12.2 - 'addEventListener'",
+              "link": "https://codepen.io/Luke-McShane/pen/JjKXgvG"
+            },
+            {
+              "title": "12.3 - Multiple Events",
+              "link": "https://codepen.io/Luke-McShane/pen/OJXNKZQ"
+            },
+            {
+              "title": "12.4 - The Event Object",
+              "link": "https://codepen.io/Luke-McShane/pen/bGepXMK"
+            },
+            {
+              "title": "12.5 - Key Events",
+              "link": "https://codepen.io/Luke-McShane/pen/WNxwVJg"
+            }
+          ]
         },
       ],
     },
@@ -159,7 +367,7 @@ export default {
           "link": "https://codepen.io/Luke-McShane/pen/abdNepB"
         },
         {
-          "title": "Knowledge Timeline",
+          "title": "04 - Knowledge Timeline",
           "link": "https://codepen.io/Luke-McShane/pen/mdVPNRL"
         },
         {
