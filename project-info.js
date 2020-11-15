b77c6bfaa3642139941e6022c6840c05240562c5
Luke-McShane <lukejsmcshane@gmail.com>
2020-11-16 05:48:36 +1100

    Added text content to projects and blogs

--- project-info.js
+++ project-info.js
@@ -2,73 +2,73 @@ export default {
   "projects": [
     {
       "title": "Kindle Quotes",
-      "overview": "1-overview Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
-      "difficulties": "1-difficulties Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
-      "lessons_learned": "1-lessons-learned Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
+      "overview": "The idea behind this project is to have all of the highlights from my Kindle stored on a web application that enables me to view the highlights in a well-presented manner. I wanted to have the ability to function by book(s), and to ensure the main focus of the site was on the single quote displayed.",
+      "difficulties": "The greatest difficulties were entailed in correctly reading and parsing the data from the Kindle, and then developing the functionality behind the popup modal that enables the user to sort which books the quotes will be extracted from.",
+      "lessons_learned": "Being my first JS-oriented personal project, I learned a lot about the language throughout this (still ongoing) project. I have learned a lot about flexbox and general design principles - such as responsiveness and layout structure - and also the importance of organizing code within a file so you’re not perplexed when revisiting the codebase in the future.",
       "site": "https://luke-mcshane.github.io/Kindle-Quotes/",
       "source": "https://github.com/Luke-McShane/Kindle-Quotes"
     },
     {
       "title": "Nature's Sojourn",
-      "overview": "2-overview Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
-      "difficulties": "2-difficulties Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
-      "lessons_learned": "2-lessons-learned Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
+      "overview": "Throughout this project I had a focus on design, and tried to put my focus onto colour and fluidity. It is a single-page site, with basic functionality, yet an engaging design that looks professional whilst retaining its sense of welcome.",
+      "difficulties": "Because I was developing this desktop-first, I had some trouble perfectly developing the media queries for tablets and mobiles, for there are a number of elements that I struggled adapting to smaller sizes.",
+      "lessons_learned": "I learned the importance of focusing on design, and, more broadly, the user’s experience when traversing a website/web application. I also learned began to appreciate some of the benefits of mobile-first design, which is a mindset I aim to employ in future projects.",
       "site": "https://luke-mcshane.github.io/NaturesSojourn-Project-5/",
       "source": "https://github.com/Luke-McShane/NaturesSojourn-Project-5"
     },
     {
       "title": "Nietzsche's Grotto",
-      "overview": "3-overview Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
-      "difficulties": "3-difficulties Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
-      "lessons_learned": "3-lessons-learned Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, vel. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi neque facilis quam, quisquam voluptatem atque impedit dolore molestiae odio! Tempore fugit nostrum optio quo ab ut quibusdam pariatur officiis! Deleniti.",
+      "overview": "This project utilizes a menu-overlay with a number of small transitions. The overall design is focused on being somewhat archaic and looming, with a deep colour scheme and scholarly background photos.",
+      "difficulties": "Ensuring all transitions worked smoothly was the most difficult aspect of this project. I found manipulating HTML and CSS to create a simple hamburger menu was more difficult than I first anticipated, and I first expected the solution to be much more elegant and less seemingly inane.",
+      "lessons_learned": "The greatest takeaway here, I believe, is not to underestimate the potential difficulty behind seemingly innocent and simple CSS features. Further, learning and implementing the fundamentals of CSS transitions is sure to benefit me in the future, especially as venture into building more complex webpages and attempt to resolve complex problems with increasingly elegant solutions.",
       "site": "https://luke-mcshane.github.io/Nietzsches-Hamburger-Project-6/",
       "source": "https://github.com/Luke-McShane/Nietzsches-Hamburger-Project-6"
     },
     {
       "title": "Shane Writes",
-      "overview": "4-overview",
-      "difficulties": "4-difficulties",
-      "lessons_learned": "4-lessons-learned",
+      "overview": "Similar to a small number of other projects, this website’s design follow the simple premise of being responsive, intuitive, and having an elegant layout and colour scheme. The website serves as a template and example for how easy web design can be, given core principles are followed.",
+      "difficulties": "As with other projects, ensuring all elements were responsive proved a challenge and, whilst the vast majority of the site is perfectly responsive, there are certain aspects that are not conducive to smaller screen sizes. Furthermore, experimenting with new colour schemes is something that continues to prove difficult, due to my lack of prior experience with design.",
+      "lessons_learned": "I have further learned the importance of developing mobile-first and, if not going with this practise, of keeping at the fore of the mind how the design is to appear on a smaller screen. Also, I learned how difficult seemingly simple things such as colour scheme can be.",
       "site": "https://luke-mcshane.github.io/ShaneWrites-Project-2/",
       "source": "https://github.com/Luke-McShane/ShaneWrites-Project-2"
     },
     {
       "title": "Hotel Shane",
-      "overview": "5-overview",
-      "difficulties": "5-difficulties",
-      "lessons_learned": "5-lessons-learned",
+      "overview": "Perhaps the most elegant of all my dummy sites, this site offers a simple yet attractive colour scheme, and isn’t flustered by any oppressive background images. The site looks professional and modern with its appropriate use of greys. In this project I attempted to bring together knowledge garnered from my other projects, in an attempt to create a very attractive aesthetic.",
+      "difficulties": "Similar to past website designs and layouts, I had little trouble with this project. The main difficulty, therefore, was simply the colour scheme and choosing the most appropriate pictures for the backgrounds of various HTML elements. Once this was figured out, however, I was presented with a well-designed website.",
+      "lessons_learned": "Here I developed my understanding of just how much of an impact simple changes to colour can make on a website. Initially I had colours with a deeper hue, and it defected the website’s aesthetic noticeable. I learned how much simple design changes can impact the overall user experience, and how important this is.",
       "site": "https://luke-mcshane.github.io/Hotel-Website-Project-1/index.html",
       "source": "https://github.com/Luke-McShane/Hotel-Website-Project-1"
     },
     {
       "title": "Etch-a-Sketch",
-      "overview": "6-overview",
-      "difficulties": "6-difficulties",
-      "lessons_learned": "6-lessons-learned",
+      "overview": "This project serves as a basis for my understanding JavaScript, and was fundamental in my understanding of basics in JavaScript. The website has little in terms of responsiveness and design, due to my focus being entirely on backend functionality.",
+      "difficulties": "Applying the little I knew of JavaScript to quite an intimidating project was the most difficult part of this project. I was hitherto unaware of how to select elements and manipulate them, so completing this project was foundational for all my future projects.",
+      "lessons_learned": "Getting to grips with the fundamentals of JavaScript was the biggest takeaway from this project - learning how to select and manipulating HTML elements, using the ‘this’ keyword with such frequency, and getting to grips with math in JavaScript.",
       "site": "https://luke-mcshane.github.io/Etch-a-Sketch/",
       "source": "https://github.com/Luke-McShane/Etch-a-Sketch"
     },
     {
       "title": "JigSure",
-      "overview": "7-overview",
-      "difficulties": "7-difficulties",
-      "lessons_learned": "7-lessons-learned",
+      "overview": "The last of my dummy sites, this surely stands out as the most vibrant, and most aesthetically displeasing. The idea behind this project was to play about with various colour schemes, and I here settled on a colour scheme that works in theory, but makes for rather an oppressive user experience.",
+      "difficulties": "Determining the colour palette was the most tedious part of this project, and the result was rather an offensive website, which served as a worthwhile learning experience.",
+      "lessons_learned": "Again, the importance of taking UX seriously and keeping design fundamentals in mind was the most significant lesson learned from this undertaking.",
       "site": "https://luke-mcshane.github.io/JigSure-Project-3/index.html",
       "source": "https://github.com/Luke-McShane/JigSure-Project-3"
     },
     {
       "title": "EdgeLedger",
-      "overview": "8-overview",
-      "difficulties": "8-difficulties",
-      "lessons_learned": "8-lessons-learned",
+      "overview": "The first of my sample websites, this site had me focus on developing a professional and elegant site that provided an intuitive experience for the user whilst simultaneously being responsive.",
+      "difficulties": "Settling on a colour palette and font was rather difficult, as was getting the initial page structures determined, for this structure would be repeated for various other projects in the future. This project also helped me get to grips with the basics of CSS, which was, at first, quite challenging.",
+      "lessons_learned": "Perhaps my biggest takeaway from this project was learning the fundamentals of CSS, and also furthering my understanding of how to correctly structure HTML pages.",
       "site": "https://luke-mcshane.github.io/EdgeLedger-Project-4/",
       "source": "https://github.com/Luke-McShane/EdgeLedger-Project-4"
     },
     {
       "title": "Kindle Quotes",
-      "overview": "9-overview",
-      "difficulties": "9-difficulties",
-      "lessons_learned": "9-lessons-learned",
+      "overview": "The idea behind this project is to have all of the highlights from my Kindle stored on a web application that enables me to view the highlights in a well-presented manner. I wanted to have the ability to function by book(s), and to ensure the main focus of the site was on the single quote displayed.",
+      "difficulties": "The greatest difficulties were entailed in correctly reading and parsing the data from the Kindle, and then developing the functionality behind the popup modal that enables the user to sort which books the quotes will be extracted from.",
+      "lessons_learned": "Being my first JS-oriented personal project, I learned a lot about the language throughout this (still ongoing) project. I have learned a lot about flexbox and general design principles - such as responsiveness and layout structure - and also the importance of organizing code within a file so you’re not perplexed when revisiting the codebase in the future.",
       "site": "https://luke-mcshane.github.io/Kindle-Quotes/",
       "source": "https://github.com/Luke-McShane/Kindle-Quotes"
     }
